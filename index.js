2a6eaa715b4c36c9728e7afdbaa0912d89334064
bahnisikha <bahnisikhadhar@gmail.com>
2022-11-17 08:32:24 +0530

    update a minor change in designinng

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const red= randomNum(0,255);
 const green=randomNum(0,255);
 const blue=randomNum(0,255);
 document.body.style.backgroundColor=`rgb( ${red}, ${green}, ${blue} )`;
-button.innerText="rgb"+ "(" +red+ "," +green+ "," +blue+ ")";
+button.innerText=`rgb( ${red}, ${green}, ${blue} )`;
 });
 
 
